f3f61e4c986d485b5e4d6318679a509b5a034252
owaisiqbal16 <m.owaisiqbal16@gmail.com>
2018-01-28 23:51:07 +0500

    Done

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -17,9 +17,21 @@ export class HomePage {
   minutes:number = 1;
   seconds:any = 30;
 
+  elapsed : any = {
+    h: '00',
+    m: '00',
+    s: '00'
+  }
+
+  overallTimer: any = false;
+
   startTime() {
     if(this.timer){
       clearInterval(this.timer)
+    }
+
+    if(this.overallTimer){
+      this.progressTimer();
     }
     
     this.timer = false;
@@ -40,6 +52,46 @@ export class HomePage {
       this.progress++;
     }, 1000)
 
+  }
+
+  progressTimer(){
+    let countDownDate = new Date();
+    this.overallTimer = setInterval(() => {
+      let now = new Date().getTime();
+
+      let distance = now - countDownDate.getTime();
+
+      this.elapsed.h = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      this.elapsed.m = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      this.elapsed.s = Math.floor((distance % (1000 * 60)) / 1000);
+
+      this.elapsed.h = this.pad(this.elapsed.h , 2);
+      this.elapsed.m = this.pad(this.elapsed.m , 2);
+      this.elapsed.s = this.pad(this.elapsed.s , 2);
+      
+    }, 1000)
+  }
+  pad(num, size){
+    let s = num+"";
+    while (s.length < size)
+    {
+      s = "0" + s
+      return s;
+    }
+  }
+
+  stopTime() {
+    clearInterval(this.overallTimer);
+    this.overallTimer = false;
+    this.timer = false;
+    this.percent = 0;
+    this.progress = 0;
+    this.elapsed = {
+      h : '00',
+      m: '00',
+      s: '00'
+    }
+    //this.insomania.allowSleepingAgain()
   }
 
 }
